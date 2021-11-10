abe10cea5d16c5d8e123e737031823a32b0e431e
PolinaKrasovskaya <polina.vorotnik@gmail.com>
2021-11-11 00:09:16 +0200

    1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,26 +1,50 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const galleryRef = document.querySelector('.gallery');
+
+const listImagesMarup = createListImagesMarkup(galleryItems);
+
+function createListImagesMarkup(images) {
+    return images.map(({original, preview, description}) => `
+    <div class="gallery__item">
+        <a class="gallery__link" href=${original}>
+            <img
+            class="gallery__image"
+            src=${preview}
+            data-source=${original}
+            alt=${description}
+            />
+        </a>
+    </div>
+    `).join('');
+
+}
+
+galleryRef.insertAdjacentHTML('beforeend', listImagesMarup)
+
+galleryRef.addEventListener('click', onOpenLargeImage);
+
+function onOpenLargeImage (event) {
+    event.preventDefault();
+
+    if(event.target.className !== 'gallery__image') {
+        return;
+    }
+
+    const imageLarge = event.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+        <img src=""/>
+    `)
+    
+    instance.element().querySelector('img').src = imageLarge;
+    instance.show();
+
+    document.addEventListener('keydown', (event) => {
+        if(event.key === 'Escape') {
+            instance.close();
+        }
+    })
+}
 
 console.log(galleryItems);
-
-Выполняй это задание в файлах 01-gallery.html и 01-gallery.js. Разбей его на несколько подзадач:
-
-Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
-Реализация делегирования на div.gallery и получение url большого изображения.
-Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
-Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
-Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
-Разметка элемента галереи
-Ссылка на оригинальное изображение должна храниться в data-атрибуте source на элементе <img>, и указываться в href ссылки. Не добавляй другие HTML теги или CSS классы кроме тех, что есть в этом шаблоне.
-
-<div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
-    />
-  </a>
-</div>
-Обрати внимание на то, что изображение обернуто в ссылку, а значит при клике по умолчанию пользователь будет перенаправлен на другую страницу. Запрети это поведение по умолчанию.
